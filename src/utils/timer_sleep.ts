a1f048b16f74f6b55609338c7c58f0408206bdcd
Aghasi G <aghasi89@gmail.com>
2020-12-25 10:07:04 +0400

    create alarm clock

--- src/utils/timer_sleep.ts
+++ src/utils/timer_sleep.ts
@@ -1,23 +1,24 @@
 import { getData, storeData } from "./local_storage"
 import player from "../services/player/PlayerServices"
-
+const intervals:any[] = []
 export const initTimerSleep = (toDo: () => void, data:any) => {
     //stex data -ov poxancel em hours:number, u minute:number, u pordzel em hamematel ete jamy khamngni es pahi jami het ropen stuge u bdi kanchver toDo()
-
-    let nowMinute= new Date().getMinutes()
-
-    const interval = setInterval(() => {
+        while(intervals.length){
+            clearInterval(intervals.shift());
+        }
+        const interval = setInterval(() => {
+            console.log('timer',interval,data);
+            
+        let nowMinute= new Date().getMinutes()
         let nowHours= new Date().getHours()
-
                 if(nowHours==data.hours){
-
-                    if(nowMinute>data.minute){
+                    if(nowMinute==data.minute){
+                        console.log("minuu");
+                        console.log('interval',interval);
+                        
+                        clearInterval(interval);
                         toDo()
-                        clearTimeout(interval);
-                    }else{
-                        console.log("nsssssssssssssssssssssssssss")
                     }
-                }else{
                     // console.log('false')
                 }
                 // if (f.getHours()==hours ) {
@@ -31,7 +32,8 @@ export const initTimerSleep = (toDo: () => void, data:any) => {
                 //
                 //
                 // }
-            }, 60000);
+            }, 5000);
+            intervals.push(interval)
     // getData('timerSleepTime').then((data) => {
     //     if (data) {
     //         console.log(";;;;;;;;",data)
